c62b549091199eb4fce6e090d7b533ddd32bdf24
Michael <michaeljosephrusiana@gmail.com>
2026-09-19 01:18:19 -0700

    Initial login system

--- frontend/app.js
+++ frontend/app.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:5000";
+const API_URL = "https://login-page-kze4.onrender.com";
 //const API_URL = "https://login-system-97ea.onrender.com";
 
 async function register() {
